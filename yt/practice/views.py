382e70eac8750a6608795ecd0e6e90002fdf5512
kumarvarun3834 <kumarvarun.vk6338@gmail.com>
2024-08-26 12:24:07 +0530

    dir like structure complete

--- yt/practice/views.py
+++ yt/practice/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(response):
     return HttpResponse("working........")
+
+def start(response):
+    return HttpResponse("start area.")
