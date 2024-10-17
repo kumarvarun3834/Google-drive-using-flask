1026654cea458b49c5d93a992a58dfb9a33f9128
kumarvarun3834 <154826151+kumarvarun3834@users.noreply.github.com>
2024-10-17 11:44:40 +0530

    dropped this project

--- yt/practice/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(response):
-    return HttpResponse("working........")
-
-def start(response):
-    return HttpResponse("start area.")
